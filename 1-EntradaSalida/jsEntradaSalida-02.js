4dc5c4bab22cec8f6b09fbe51afc8bbbbcf85886
Rocioaylenglz <rocioaylenglz@gmail.com>
2023-01-30 15:28:23 -0300

    primer-commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,13 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
-}
+function mostrar() {
+  // 1. genero un espacio de memoria (variable) y le guardo un valor (asignación)
+  let nombre; // genero la variable
 
+  //nombre= "rocio"; // asignación de string
+  nombre = prompt("ingrese su nombre");
+
+  //mostrar por alert
+  alert(nombre);
+}
